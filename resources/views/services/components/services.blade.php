7012eb5732867ee6d0f41da6f6f5388652f4c373
فهیمه السادات سجادی <fahimehsjd77@gmail.com>
2024-01-29 16:53:09 +0330

    service change finish

--- resources/views/services/components/services.blade.php
+++ resources/views/services/components/services.blade.php
@@ -1,5 +1,4 @@
 <section class="container my-20">
-
     <div>
         <div class="grid grid-cols-1 xl:grid-cols-3 sm:grid-cols-2 gap-10">
             <div class=" px-8  py-10 rounded-lg shadow">
@@ -24,20 +23,21 @@
                     looking to expand their global presence or enter new markets.</p>
             </div>
         </div>
-        <div class="grid grid-cols-1 sm:grid-cols-6 gap-10 mt-10">
-            <div class=" px-8  py-10 rounded-lg shadow col-start-2 col-span-2">
-                <image class="mx-auto" src="{{ asset("storage/website/services/vector1.svg") }}" alt="vector1"></image>
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-10 mt-10">
+            <div class=" px-8  py-10 rounded-lg shadow col-start-2 ">
+                <img class="mx-auto" src="https://diacocenter.com/storage/website/services/vector1.svg" alt="vector1">
                 <h6 class="mt-8 mb-3 text-lg font-semibold">automation making contract by aI</h6>
                 <p class="text-sm text-justify">AI-driven contract automation can significantly increase efficiency,
                     reduce errors, and enhance compliance in the contract management process.</p>
             </div>
-            <div class=" px-8  py-10 rounded-lg shadow col-span-2">
-                <image class="mx-auto" src="{{ asset("storage/website/services/vector2.svg") }}" alt="vector2"></image>
+            <div class="px-8 py-10 rounded-lg shadow ">
+                <img class="mx-auto" src="https://diacocenter.com/storage/website/services/vector2.svg" alt="vector2">
                 <h6 class="mt-8 mb-3 text-lg font-semibold">International warehousing services</h6>
                 <p class="text-sm text-justify">International warehousing services are an integral part of the global
                     supply chain, helping businesses manage their inventory, streamline distribution, reduce shipping
                     costs, and enhance customer service.</p>
             </div>
         </div>
+
     </div>
 </section>
